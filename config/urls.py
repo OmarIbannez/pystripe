8b527c77a788ec8ec97684c778426b2da058cd48
Omar Ibañez <omaribannez@gmail.com>
2016-11-23 14:46:57 -0800

    Stripe webhook

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from webapp.views import WebhokView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', WebhokView.as_view(), name='webhok'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+import json
+import stripe
+from django.conf import settings
 
-# Create your views here.
+
+stripe.api_key = settings.STRIPE_API_KEY
+
+
+class WebhokView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(WebhokView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        event_json = json.loads(request.body)
+        event = stripe.Event.retrieve(event_json["id"])
+        print(event)
+
+        return HttpResponse(status=200)
